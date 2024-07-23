c99e77e0b0b54b955fe6017d49ec71af1b09f0e4
Taymaz Türkmen <t.tuerkmen@tu-braunschweig.de>
2024-07-23 13:36:36 +0200

    i3 workspace updated

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,13 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
+# for multimonitor
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload example & disown
   done
 else
-  polybar --reload main &
+  polybar --reload example & disown
 fi
-
-echo "Polybar launched..."
